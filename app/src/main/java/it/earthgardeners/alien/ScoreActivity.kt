1c17c4d3567d37129246cb2876852648a25201ea
indapp <indapp.info@gmail.com>
2019-07-05 10:59:17 +0200

    Gradle libraries & Activities created

---
+++ app/src/main/java/it/earthgardeners/alien/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package it.earthgardeners.alien
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
